8dc937c68359720e74f22a6f85d2f73c0c344e25
Dollardudez <robclancy.98@gmail.com>
2021-09-21 17:33:14 -0500

    Added functionality for get request '/' to the server, the server sends to the client the properties information in the form of a json string

--- server/app.js
+++ server/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const Properties =  require('./db/mockdata');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+/**
+ * Express middleware that parses the incoming request object.
+ * It is used to create a JS object that we can interact with.
+ * It is really only used for POST and PUT requests, not GETS.
+ */
+app.use(express.json());
+
+
+
+//Send mock properties to the client as a JSON object, data
+app.get("/", (req, res)=>{
+  try{
+      const allProperties = Properties;
+      //log the properties to the console for testing purposes
+      console.log(allProperties);
+      //status code 200 is ok
+      res.status(200).json({
+      status: "success", 
+      data: {
+          properties: allProperties
+      }
   })
+  }
+  catch(err){
+      console.log(err);
+  }
+});
   
 module.exports = app;
---
+++ server/db/mockdata.js
@@ -0,0 +1,66 @@
+// db/mockdata.js
+
+const Properties = [
+    {
+      title: "West Ridge Apartments",
+      bed: "2",
+      bath: "1",
+      type: "apartment",
+      square: "900",
+      availabilities: "2",
+      price: "750",
+      map: "&center=-33.8569,151.2152&maptype=roadmap"
+    },
+    {
+        title: "West Ridge Apartments",
+        bed: "3",
+        bath: "2.5",
+        type: "house",
+        square: "1500",
+        price: "1000",
+        availabilities: "",
+        map: "&center=-33.8569,151.2152&maptype=roadmap"
+    },
+    {
+        title: "West Ridge Apartments",
+        bed: "2",
+        bath: "1",
+        type: "apartment",
+        square: "1000",
+        availabilities: "2",
+        price: "800",
+        map: "&center=-33.8569,151.2152&maptype=roadmap"
+    },
+    {
+        title: "West Ridge Apartments",
+        bed: "2",
+        bath: "1",
+        type: "apartment",
+        square: "900",
+        price: "800",
+        availabilities: "2",
+        map: "&center=-33.8569,151.2152&maptype=roadmap"
+    },
+    {
+        title: "West Ridge Apartments",
+        bed: "2",
+        bath: "1",
+        type: "apartment",
+        square: "900",
+        price: "800",
+        availabilities: "2",
+        map: "&center=-33.8569,151.2152&maptype=roadmap"
+    },
+    {
+        title: "West Ridge Apartments",
+        bed: "2",
+        bath: "1",
+        type: "apartment",
+        square: "900",
+        price: "800",
+        availabilities: "2",
+        map: "&center=-33.8569,151.2152&maptype=roadmap"
+    }
+  ];
+
+  module.exports = { Properties };
